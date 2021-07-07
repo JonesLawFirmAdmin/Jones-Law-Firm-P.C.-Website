9e3de995dd2963ded12040f9b03f6d54fa7c7bbf
JonesLawFirmAdmin <86199485+JonesLawFirmAdmin@users.noreply.github.com>
2021-07-07 09:00:30 -0500

    Style Changes

---
+++ function_about.js
@@ -0,0 +1,33 @@
+// Code By Webdevtrick ( https://webdevtrick.com )
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+
+$('#fullpage').fullpage({
+  sectionsColor: ['white', 'var(--metallic-sunburst)', 'var(--sage)', 'var(--metallic-sunburst)', 'var(--sage)', 'var(--metallic-sunburst)'],
+  sectionSelector: '.vertical-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection'],
+  menu: '#menu',
+
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'var(--transparent-black)');
+    $nav.css('background', 'var(--more-transparent-black)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+
+});
---
+++ function_contact.js
@@ -0,0 +1,27 @@
+// Code By Webdevtrick ( https://webdevtrick.com )
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+
+$('#fullpage').fullpage({
+
+
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'var(--transparent-black)');
+    $nav.css('background', 'var(--more-transparent-black)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+
+});
